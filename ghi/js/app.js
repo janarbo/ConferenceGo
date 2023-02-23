c3518b1eb1bc4d00cdf90f9761eed9b4a64120ff
Janar Bo <janarboke@gmail.com>
2023-02-22 19:56:31 -0800

    error handling

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,10 +1,11 @@
-function createCard(name, description, pictureUrl, starts, ends) {
+function createCard(name, description, pictureUrl, starts, ends, location) {
     return `
     <div class="col-sm">
         <div class="card shadow-lg mt-3">
             <img src="${pictureUrl}" class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">${name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${location}</h6>
                 <p class="card-text">${description}</p>
             </div>
         </div>
@@ -14,13 +15,29 @@ function createCard(name, description, pictureUrl, starts, ends) {
     </div>
 `;
   }
+
+  function alert(){
+    return`
+    <div class="alert alert-danger" role="alert">
+        <h4 class="alert-heading">There was an error loading the conference data. </h4>
+        <p></p>
+        <p>Please try again later.</p>
+    </div>
+    `
+  }
+
+
+
   window.addEventListener('DOMContentLoaded', async () => {
     const url = 'http://localhost:8000/api/conferences/';
     try {
       const response = await fetch(url);
       if (!response.ok) {
         // Figure out what to do when the response is bad
-        throw new Error('Response not ok');
+        const body = document.querySelector('main');
+        const error = alert();
+        body.innerHTML += error;
+  ;
       } else {
         const data = await response.json();
         for (let conference of data.conferences) {
@@ -28,13 +45,14 @@ function createCard(name, description, pictureUrl, starts, ends) {
           const detailResponse = await fetch(detailUrl);
           if (detailResponse.ok) {
             const details = await detailResponse.json();
-            const title = details.conference.name;
+            const name = details.conference.name;
             const description = details.conference.description;
             const pictureUrl = details.conference.location.picture_url;
             const starts = new Date(details.conference.starts);
             const ends = new Date(details.conference.ends);
-            const html = createCard(title, description, pictureUrl,
-            starts.toLocaleDateString('en-US'), ends.toLocaleDateString('en-US'));
+            const location = details.conference.location.name;
+            const html = createCard(name, description, pictureUrl,
+            starts.toLocaleDateString('en-US'), ends.toLocaleDateString('en-US'), location);
             console.log(html);
             const row = document.querySelector('.row');
             row.innerHTML += html;
@@ -43,7 +61,11 @@ function createCard(name, description, pictureUrl, starts, ends) {
       }
     } catch (e) {
       // Figure out what to do if an error is raised
+
+      const body = document.querySelector('main');
+      const error = alert();
+      body.innerHTML += error;
       console.error('error', e);
+
     }
   });
-
